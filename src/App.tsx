4bed676d2745d6f195c0bccffdda521a7fb5819f
timothy timbol <ttimbol08@gmail.com>
2022-08-13 17:05:39 +0400

    added shop page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { HashRouter, Route, Routes } from "react-router-dom";
 //test
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
+import Shop from "./pages/Shop";
 
 function App() {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </HashRouter>
---
+++ src/pages/Shop.tsx
@@ -0,0 +1,15 @@
+//components
+import Nav from "../components/Nav"
+import Footer from "../components/Footer"
+
+const Shop:React.FC = () => {
+    return (
+        <>
+        <Nav />
+        <h1>SHOP PAGE</h1>
+        <Footer />
+        </>
+    )
+}
+
+export default Shop;
